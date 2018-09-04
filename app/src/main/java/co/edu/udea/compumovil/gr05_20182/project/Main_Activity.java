044cc15c87c587ff0b4f1b1e43f5e2704d56271a
AndyVerona123 <andres.carrascal@udea.edu.co>
2018-09-04 00:25:42 -0500

    Agregando imagenes

--- app/src/main/java/co/edu/udea/compumovil/gr05_20182/project/Main_Activity.java
+++ app/src/main/java/co/edu/udea/compumovil/gr05_20182/project/Main_Activity.java
@@ -13,7 +13,7 @@ public class Main_Activity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        //Prueba
     }
 
 }
